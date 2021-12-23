1d43643d21f64e41e57919d98cd72502a5a853c0
MateushTorres <61062377+MateushTorres@users.noreply.github.com>
2021-12-23 19:27:56 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,10 @@
-'''
-from selenium import webdriver
-from selenium.webdriver import chrome
-from selenium.webdriver.chrome import options
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
-import pandas as pd
-'''
 import os
 import time
 import json
 import pandas as pd
+import configparser
+import io
+import subprocess
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -22,15 +15,45 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import TimeoutException
 
+import traceback #tirar
 
-opt = Options()
-opt.add_experimental_option("debuggerAddress", "localhost:7777")
-driver = webdriver.Chrome(executable_path="C:\\Users\\mateu\\OneDrive\\Documentos\\rpa_tim\\chromedriver.exe", chrome_options=opt)
-driver.get("https://siebelcrm.timbrasil.com.br/mobile_ptb/start.swe?SWECmd=GotoView&SWEView=TIM+Vendas+21+-+Change+Position+View&SWERF=1&SWEHo=siebelcrm.timbrasil.com.br&SWEBU=1&SWEApplet0=TIM+Vendas+21+-+GFA+Change+Position+Form+Applet&SWERowId0=VRId-0")
+print( '--------- iniciando ----------')
+GRAVAR_A_CADA = 10
+caminho = ''
+
+def gravar():
+    tabela.to_csv(saida)  
+
+print ('# Lendo config.ini')
+# Load the configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+GRAVAR_A_CADA = int(config["bot"]["gravar_a_cada"])
+caminho = config["bot"]["caminho_webdriver"]
+endereco = config["bot"]["endereco"]
+arquivo = config["bot"]["arquivo"]
+saida = config["bot"]["arquivo_saida"]
+
+
+print('# Gravar a cada', GRAVAR_A_CADA)
+print('# Caminho', caminho)
+print('# Endereco', endereco)
+print('# Arquivo', arquivo)
+
+print('# Carregando tabela ' + arquivo)
+tabela = pd.read_excel(arquivo, index_col=0)
+
+#opt = Options()
+#opt.add_experimental_option("debuggerAddress", "localhost:7777")
+driver = webdriver.Chrome(executable_path=caminho)
+
+driver.get(endereco)
 driver.maximize_window()
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 15)
 
-time.sleep(15)
+print('# Aguardando login.... 30 segs')
+time.sleep(30)
 
 driver.get(driver.current_url)
 driver.refresh()
@@ -39,35 +62,56 @@ wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="TIM_Buttom_Alt_
 
 time.sleep(10)
 
+print('# Aguardando seleção da mesa.... 10 segs')
 driver.find_element(By.ID,"tb_-1").click() 
 
-tabela = pd.read_excel("TIM TESTE.xlsx", index_col=0)
-
+i = 0
 
 for index, row in tabela.iterrows():
+    print("# Número: {}".format(index))
+    i += 1
+
     wait.until(EC.visibility_of_element_located((By.NAME, "s_2_1_16_0")))
     driver.find_element(By.NAME, "s_2_1_16_0").click()
     driver.find_element(By.NAME, "s_2_1_16_0").send_keys(str(index))
     driver.find_element(By.ID,"s_2_1_4_0_Ctrl").click() 
 
+    tem_alerta= ""
     try:
         WebDriverWait(driver, 3).until(EC.alert_is_present())
         alert = driver.switch_to.alert
         alert.accept()
-        print("Alerta aceito")
+        print("# Alerta aceito")
+        tem_alerta = "Sim"
     except TimeoutException:
-        print("")
+        pass
         
     time.sleep(1)
-    wait.until(EC.visibility_of_element_located((By.ID,"1TIM_Plano"))).get_attribute("value")
-    plano = driver.find_element_by_xpath('//*[@id="1TIM_Plano"]').text
-    status =  driver.find_element(By.ID,"1TIM_StatusAcesso").text
+    plano = ''
+    status = ''
 
-    print( plano, status)
+    try:
+        wait.until(EC.visibility_of_element_located((By.ID,"1TIM_Plano"))).get_attribute("value")
+        plano = driver.find_element_by_xpath('//*[@id="1TIM_Plano"]').text
+        status =  driver.find_element(By.ID,"1TIM_StatusAcesso").text
 
-    tabela.at[index, 'Plano'] = plano
-    tabela.at[index, 'Status'] = status
-    
-    driver.find_element(By.ID,"tb_-1").click() 
+    except Exception as e:
+        print("# Erro ao localizar plano do número {}".format( index))    
+        #traceback.print_exc()
+        driver.find_element(By.ID,"tb_-1").click() 
+        time.sleep(5)
+    else:
+        print( '# Dados capturados ', index, plano, status)
 
-tabela.to_csv('TIM_TESTE.csv')  
+        tabela.at[index, 'Plano'] = plano
+        tabela.at[index, 'Status'] = status
+        tabela.at[index, 'Alerta'] = tem_alerta
+        
+        if i % GRAVAR_A_CADA == 0:
+            print('# arquivo gravado.')
+            gravar()
+
+        driver.find_element(By.ID,"tb_-1").click() 
+
+print(' --------- Fim da Execucao ----------')
+
